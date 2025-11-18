4c973502dbae208040b9d3e35ec62e2d28d43afd
fatimahere7 <fatimamansoor529@gmail.com>
2025-11-18 15:10:18 +0500

    Fund me

---
+++ Fundme/FundMe.sol
@@ -0,0 +1,57 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
+
+
+contract FundMe{
+    using PriceConverter for uint256;
+    uint256 public minimumUsd = 5e18;
+
+    
+    mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
+    
+    function fund() public payable{
+        //Allow users to send $
+        //Have a minimum $ sent
+        //how we send Eth to this contract? 
+        require(msg.value.getConversionRate() >= minimumUsd , "didnt send enough ETH");  
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+    }
+
+
+
+    // function withdraw() public {} 
+
+}
+
+// interface AggregatorV3Interface {
+//   function decimals() external view returns (uint8);
+//   function description() external view returns (string memory);
+//   function version() external view returns (uint256);
+
+//   function getRoundData(uint80 _roundId)
+//     external
+//     view
+//     returns (
+//       uint80 roundId,
+//       int256 answer,
+//       uint256 startedAt,
+//       uint256 updatedAt,
+//       uint80 answeredInRound
+//     );
+
+//   function latestRoundData()
+//     external
+//     view
+//     returns (
+//       uint80 roundId,
+//       int256 answer,
+//       uint256 startedAt,
+//       uint256 updatedAt,
+//       uint80 answeredInRound
+//     );
+// }
