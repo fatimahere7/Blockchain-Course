65d3f37c0e705bfdae187f905f0de397d8c64d96
fatimahere7 <fatimamansoor529@gmail.com>
2025-11-20 13:22:02 +0500

    Fundme completed

--- Fundme/FundMe.sol
+++ Fundme/FundMe.sol
@@ -4,29 +4,105 @@ pragma solidity ^0.8.18;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
 
+error NotOwner();
+error NotEnoughETH();
 
 contract FundMe{
-    using PriceConverter for uint256;
-    uint256 public minimumUsd = 5e18;
+    using PriceConverter for uint256; 
+    uint256 public constant MINIMUM_USD = 5e18; //gas optimized using constant
 
     
     mapping(address => uint256) public addressToAmountFunded;
     address[] public funders;
-    
+    address public immutable i_owner;
+    constructor(){
+        i_owner = msg.sender;
+    }
+
+
+    modifier callMeRightAway() {
+        // require(msg.sender == i_owner , "Only owner have access to this");
+        if(msg.sender != i_owner){
+            revert NotOwner();
+        }
+        _;
+    }
+
+
     function fund() public payable{
         //Allow users to send $
         //Have a minimum $ sent
         //how we send Eth to this contract? 
-        require(msg.value.getConversionRate() >= minimumUsd , "didnt send enough ETH");  
+        //require(msg.value.getConversionRate() >= MINIMUM_USD , "didnt send enough ETH");  
+        if (msg.value.getConversionRate() < MINIMUM_USD){
+            revert NotEnoughETH();
+        }
         funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+        addressToAmountFunded[msg.sender] += msg.value;
     }
 
 
 
-    // function withdraw() public {} 
+    function withdraw() public callMeRightAway{
+        for(uint256 funderIndex; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        //reset the funders array
+        funders = new address[](0);
+
+        //three differe way to transfer the find
+        //transfer , Send , Call
+
+         //transfer
+        // payable(msg.sender).transfer(address(this).balance);
+
+
+        //send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess , "Sending Failed");
+
+
+        //call
+        //returns two variable bool , return value from functions
+        (bool callSuccess ,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess , "Call failed");
+    } 
+
+    // what if someone send Eth to contract without calling the fund Function?
+    //Recieve
+    //fallback
+    receive() external payable {
+        fund();
+    }
+    fallback() external payable {
+        fund();
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // interface AggregatorV3Interface {
 //   function decimals() external view returns (uint8);
@@ -38,7 +114,7 @@ contract FundMe{
 //     view
 //     returns (
 //       uint80 roundId,
-//       int256 answer,
+//       int256 answer,  // price 
 //       uint256 startedAt,
 //       uint256 updatedAt,
 //       uint80 answeredInRound
--- Fundme/PriceConverter.sol
+++ Fundme/PriceConverter.sol
@@ -6,15 +6,16 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interf
 library PriceConverter{
    
 
-    function getPrice() internal view returns (uint256) {
+    function getPrice() internal  view returns (uint256) {
         //0x694AA1769357215DE4FAC081bf1f309aDC325306
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (, int256 price,,,) = priceFeed.latestRoundData();
-        return uint256(price);
+        (, int256 price,,,) = priceFeed.latestRoundData();  //destructuring assignment (tuple destructuring)
+        return uint256(price * 1e10);
     }
+    //dollars in 1ETH ?
     function getConversionRate(uint256 ethAmount) internal view returns (uint256){
        uint256 ethPrice = getPrice();
-       uint256 ethAmountInUsd = (ethPrice *ethAmount)/ 1e18;
+       uint256 ethAmountInUsd = (ethPrice * ethAmount)/ 1e18;
        return  ethAmountInUsd;
     }
 
