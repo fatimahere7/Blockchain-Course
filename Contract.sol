0c51fbea473e673a6d960209c1970d57b7487226
fatimahere7 <fatimamansoor529@gmail.com>
2025-12-04 15:07:08 +0500

    Create wallet contract with basic functions

---
+++ Contract.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.17;
+contract wallet{
+    //read
+    string public name="wallet";
+    uint num;
+
+    function getName() public view returns(string memory){
+        return name;
+    } 
+    //write
+    function setValue(uint _num) public {
+        num=_num;
+    }
+    //read
+     function getValue() public view returns(uint){
+        return num;
+    }
+    //write
+    function sendEthContract() public payable{
+        
+    }
+    //read
+    function contractBalance() public view returns(uint){
+        return address(this).balance;
+    }
+    //write
+    function sendEthUser(address _user) public payable{
+       payable(_user).transfer(msg.value);
+    }
+    //read
+    function accountBalance(address _address) public view returns(uint){
+        return (_address).balance;
+    }
+}
+//0x417997417dd95f45bb4986abff5dfae5b5b0a34a
